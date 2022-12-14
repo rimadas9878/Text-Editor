4c91424c87ae112070f8b265a976bb96d3904fb1
Rima Das <rimadas.automationtester@gmail.com>
2022-12-13 18:37:33 -0800

    Working code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,45 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('UPDATE from the database', content);
+
+  // Create a connection to the database database and version we want to use.
+  const jadeDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const jd = jadeDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = jd.objectStore('jate');
+
+  // Use the .put() method to get all data in the database.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  const jd = jateDb.transaction('jate', 'readonly');
+
+  const store = jd.objectStore('jate');
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -28,6 +28,7 @@ if ('serviceWorker' in navigator) {
   // register workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
+  console.log('Service worker registered')
 } else {
   console.error('Service workers are not supported in this browser.');
 }
