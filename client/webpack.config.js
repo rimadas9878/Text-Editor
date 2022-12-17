6b38e70af201089585dd976ffad6b21e7fa1c0b1
Rima Das <rimadas.automationtester@gmail.com>
2022-12-16 22:50:23 -0800

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "JATE",
+        title: "J.A.T.E",
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
